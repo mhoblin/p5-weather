6f9c8e615eb222fc775c9d4db2b2c7e939bd9c44
mhoblin <monicahoblin@eagles.ewu.edu>
2014-12-08 19:36:22 -0800

    added buttons

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,46 @@ var spokane = '99204'
       $("#weather").html('<p>'+error+'</p>');
     }
   });
+
+  // REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com/
+
+// On click button, get zip, then run Simple Weather
+$('.button').on('click', function() {
+  
+  // 1. Get & store entered zipcode
+  var zipcode = $('#getWeather').val();
+  
+  // 2. Pass weather into _simpleWeather()_ object
+  $.simpleWeather({
+    
+    location: zipcode,
+  
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+
+      // See console for all properties of object
+      console.log(weather);
+    },
+  
+    error: function(error) {
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
+  
+  // 3. Reset input value
+  $('#getWeather').val('');
+  
+});
+
+//When "enter zipcode is selected "
+
